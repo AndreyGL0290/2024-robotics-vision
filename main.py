f09d6fd0df842726312c170379709dcdaf3fa937
Joseph Hadad <91989021+josephhadad05@users.noreply.github.com>
2024-03-08 17:44:44 -0500

    Runs a Pipeline for communication with a robot

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import cv2
-from vision_processing.camera import Camera
+from run import PipelineRunner
 
 if __name__ == '__main__':
-    stream = cv2.VideoCapture(1)
-    camera = Camera(stream)
-    while True:
-        cv2.imshow('camera stream', camera.getNote())
-        # camera.getNote()
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
-            break
-    stream.release()
-    cv2.destroyAllWindows()
+    PipelineRunner().run()
