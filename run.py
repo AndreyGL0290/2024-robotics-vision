78e9b0d75a4927bd6dfda216b5e0dd24d48ccf85
Joseph Hadad <91989021+josephhadad05@users.noreply.github.com>
2024-03-08 17:26:56 -0500

    Added class PipelineRunner for constant communication with the robot

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-class PipelineRunner():
-    def __init__(self) -> None:
-        pass
+import time
+import cv2
+
+from vision_processing import Camera, NetworkCommunication
+
+class PipelineRunner:
+    def __init__(
+            self,
+            communications: NetworkCommunication = NetworkCommunication(),
+            camera: Camera = None
+    ):
+        self.communications = communications
+        if not camera: self.camera = cv2.VideoCapture(1)
+        else: self.camera = camera
+
+    def run(self, num_of_cycles: int = -1):
+        cycle_count = 0
+        while cycle_count != num_of_cycles:
+            cycle_count += 1
+
+            timestamp = time.time()
+
+            # Processing frame
+            PIDValue = self.camera.getNote()
+
+            if PIDValue:
+                self.communications.send_rotation(PIDValue)
+
+            fps = 1 / ((time.time() - timestamp) or 1e-9)  # prevent divide-by-zero
+            print(f"Cycle {cycle_count} was successful.\nFPS: {round(fps, 3)}")
+
+if __name__ == "__main__":
+    PipelineRunner().run()
